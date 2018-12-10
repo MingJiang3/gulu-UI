9ac9cc7015b7d520a84d8252cbce6d1579437578
limingjiang <457918455@qq.com>
2018-12-10 14:21:29 +0800

    完成UI官网模型

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    title: 'Gulu  UI',
+    description: 'this is ui',
+    themeConfig: {
+        nav: [
+            {text: '指南', link: '/'},
+            {text: '配置', link: ''},
+            {text: '更多', link: ''},
+        ],
+        sidebar: [
+            {
+                title: '入门',
+                children: ['/install/', '/started/']
+            },
+            {
+                title: '组件',
+                children: ['']
+            }
+        ]
+    }
+}
